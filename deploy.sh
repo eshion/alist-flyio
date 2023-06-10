6e0e06e0d5b3d60db700639a31d9459835acb1d3
eshion <eshion@qq.com>
2023-06-10 21:41:36 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -39,7 +39,7 @@ processes = []
   auto_rollback = true
 [[services]]
   http_checks = []
-  internal_port = 2233
+  internal_port = 5244
   # processes = ["app"]
   protocol = "tcp"
   script_checks = []
