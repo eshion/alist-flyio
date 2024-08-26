10986bda1b0064189ecd59bfea548bc58c3d02d4
zhangyusheng <zhangyusheng@mucfc.com>
2024-08-26 19:56:30 +0800

    fix: [34905]合并代码

--- alist.sh
+++ alist.sh
@@ -6,33 +6,33 @@ umask ${UMASK}
 # 创建配置文件目录
 mkdir -p /opt/alist/data/
 
-cat >/opt/alist/data/config.json <<EOF
-{
-  "address": "0.0.0.0",
-  "port": 5244,
-  "site_url": "$SITE_URL",
-  "database": {
-    "type": "$DATABASE",
-    "user": "$SQLUSER",
-    "password": "$SQLPASSWORD",
-    "host": "$SQLHOST",
-    "port": $SQLPORT,
-    "name": "$SQLNAME",
-    "db_file": "data.db",
-    "table_prefix": "x_",
-    "ssl_mode": "prefer"
-  },
-  "scheme": {
-    "https": false,
-    "cert_file": "",
-    "key_file": ""
-  },
-  "cache": {
-    "expiration": 60,
-    "cleanup_interval": 120
-  }
-}
-EOF
+# cat >/opt/alist/data/config.json <<EOF
+# {
+#   "address": "0.0.0.0",
+#   "port": 5244,
+#   "site_url": "$SITE_URL",
+#   "database": {
+#     "type": "$DATABASE",
+#     "user": "$SQLUSER",
+#     "password": "$SQLPASSWORD",
+#     "host": "$SQLHOST",
+#     "port": $SQLPORT,
+#     "name": "$SQLNAME",
+#     "db_file": "data.db",
+#     "table_prefix": "x_",
+#     "ssl_mode": "prefer"
+#   },
+#   "scheme": {
+#     "https": false,
+#     "cert_file": "",
+#     "key_file": ""
+#   },
+#   "cache": {
+#     "expiration": 60,
+#     "cleanup_interval": 120
+#   }
+# }
+# EOF
 
 cd /opt/alist
 # ./alist server
